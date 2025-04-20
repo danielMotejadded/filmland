a2a7928c511091abb5541ba6fad62da3bc515a98
Daniel Motejadded <danimotejadded@gmail.com>
2025-04-20 16:36:56 +0200

    Updates

---
+++ precache-manifest.ecdc8ddec7cc8f7652e6e8792535bb15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da815ab4c4f5687f7983eae527807dc9",
+    "url": "/filmland/index.html"
+  },
+  {
+    "revision": "52687bf628d12dbe0432",
+    "url": "/filmland/static/css/main.2b84f94c.chunk.css"
+  },
+  {
+    "revision": "00ade8cb2dfc500319e8",
+    "url": "/filmland/static/js/2.7ec5e050.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/filmland/static/js/2.7ec5e050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52687bf628d12dbe0432",
+    "url": "/filmland/static/js/main.26fd3d03.chunk.js"
+  },
+  {
+    "revision": "d6123d6b6905218ac161",
+    "url": "/filmland/static/js/runtime-main.97a86971.js"
+  },
+  {
+    "revision": "095eeb8648466cccedd48fd1577605ce",
+    "url": "/filmland/static/media/search.095eeb86.svg"
+  }
+]);
